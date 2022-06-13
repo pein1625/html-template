b472da1db417e887ca3af869f2366b46b3666c4e
hapk <96176933+hapk-dls@users.noreply.github.com>
2022-06-13 22:37:55 +0700

    hotfix

--- source/js/_common.js
+++ source/js/_common.js
@@ -19,3 +19,35 @@ $(function() {
 $(function() {
   new WOW().init();
 });
+
+$(function() {
+  const $container = $('.project-sync');
+
+  $('.img-slider, .info-slider__title, .info-slider__link')
+  .on('mouseover', function() {
+    $container.addClass('active');
+  })
+  .on('mouseleave', function() {
+    $container.removeClass('active');
+  });
+});
+
+$(function() {
+  const $window = $(window);
+  const $content = $('.intro-slider__content');
+  const $frame = $('.intro-slider__frame');
+
+  if (!$frame.length || $window.width() < 1200) return;
+
+  $window.on('scroll', calcPos);
+
+  function calcPos() {
+    const windowHeight = $window.width();
+    const scrollTop = $window.scrollTop();
+    const framePos = $frame.offset().top - windowHeight * 2 / 3 - scrollTop + $frame.outerHeight() / 2;
+    const contentPos = $content.offset().top - windowHeight * 2 / 3 - scrollTop + $content.outerHeight() / 2;
+
+    $content.css('transform', `translateX(-${contentPos > 0 ? Math.floor(contentPos / 2) : 0}px)`)
+    $frame.css('transform', `translateX(${framePos > 0 ? Math.floor(framePos / 2) : 0}px)`)
+  }
+});
