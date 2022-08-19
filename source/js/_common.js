ff4dbe113845c8591fb60aac88d8049036c06cb4
hapk <96176933+hapk-dls@users.noreply.github.com>
2022-08-19 23:34:24 +0700

    save

--- source/js/_common.js
+++ source/js/_common.js
@@ -3,5 +3,15 @@ $(function() {// just grab a DOM element
   if (!element) return;
 
   // And pass it to panzoom
-  panzoom(element)
+  panzoom(element, {
+    onDoubleClick: function(e) {
+      // `e` - is current double click event.
+      return false; // tells the library to not preventDefault, and not stop propagation
+    },
+    onTouch: function(e) {
+      // `e` - is current touch event.
+
+      return false; // tells the library to not preventDefault.
+    }
+  })
 });
