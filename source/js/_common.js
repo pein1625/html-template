39057f589a95bb24cbb3ce0317aba4950ef00e95
hapk <96176933+hapk-dls@users.noreply.github.com>
2022-08-25 00:00:26 +0700

    update

--- source/js/_common.js
+++ source/js/_common.js
@@ -1,17 +1,20 @@
 $(function() {// just grab a DOM element
-  var element = document.querySelector('#zoom-scene');
-  if (!element) return;
+  $('.zoom-scene').each(function() {
+    var element = this;
+    if (!element) return;
 
-  // And pass it to panzoom
-  panzoom(element, {
-    onDoubleClick: function(e) {
-      // `e` - is current double click event.
-      return false; // tells the library to not preventDefault, and not stop propagation
-    },
-    onTouch: function(e) {
-      // `e` - is current touch event.
+    // And pass it to panzoom
+    panzoom(element, {
+      onDoubleClick: function(e) {
+        // `e` - is current double click event.
+        return false; // tells the library to not preventDefault, and not stop propagation
+      },
+      onTouch: function(e) {
+        // `e` - is current touch event.
 
-      return false; // tells the library to not preventDefault.
-    }
-  })
+        return false; // tells the library to not preventDefault.
+      }
+    })
+
+  });
 });
