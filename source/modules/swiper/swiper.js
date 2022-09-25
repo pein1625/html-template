9c0184d5251cdeda13949423be3b313522c712c4
hapk <96176933+hapk-dls@users.noreply.github.com>
2022-09-26 02:50:36 +0700

    add home page

--- source/modules/swiper/swiper.js
+++ source/modules/swiper/swiper.js
@@ -23,3 +23,49 @@ function addSwiper(selector, options = {}) {
     return new Swiper($sliderEl, options);
   });
 }
+
+$(function() {
+  addSwiper('.source-slider', {
+    loop: true,
+    navigation: true,
+    spaceBetween: 16,
+    speed: 500,
+    slidesPerView: 2,
+    breakpoints: {
+      576: {
+        slidesPerView: 3
+      },
+      768: {
+        slidesPerView: 4
+      },
+      992: {
+        slidesPerView: 5
+      },
+      1200: {
+        slidesPerView: 6
+      }
+    }
+  });
+});
+
+$(function() {
+  const videoThumbSlider = addSwiper('.video-thumb-slider', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    navigation: true,
+    speed: 400,
+    breakpoints: {
+      576: {
+        slidesPerView: 3,
+        spaceBetween: 16
+      }
+    }
+  })[0];
+
+  const videoSlider = addSwiper('.video-slider', {
+    speed: 400,
+    thumbs: {
+      swiper: videoThumbSlider,
+    },
+  })[0];
+});
