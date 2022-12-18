4171b7379831cd5fa5f68567a2d7642c29ae66d8
hapk <96176933+hapk-dls@users.noreply.github.com>
2022-12-18 11:39:04 +0700

    add header

--- source/js/_common.js
+++ source/js/_common.js
@@ -5,3 +5,19 @@ $(function() {
     $(this).toggleClass('active').siblings('.departure__body').slideToggle('fast');
   });
 });
+
+$(function() {
+  const $window = $(window);
+  const $header = $('.header');
+
+  $window.on('scroll', function() {
+    if ($window.width() < 1200) return false;
+
+    if ($window.scrollTop() >= 122) {
+      $header.addClass('is-fixed');
+    }
+    else {
+      $header.removeClass('is-fixed');
+    }
+  });
+});
