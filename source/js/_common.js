460d8283c11c5b8d0720e81b03d959c24d5f5d21
hapk <96176933+hapk-dls@users.noreply.github.com>
2023-02-22 22:49:29 +0700

    hotfix

--- source/js/_common.js
+++ source/js/_common.js
@@ -55,7 +55,7 @@ function floating() {
 
   $(window).on("scroll resize", function () {
     const $container = $('.floating-container');
-    const paddingLeft = Number($container.css('padding-left').replaceAll(/\D/g, ''));
+    const paddingLeft = Number($container.css('padding-left')?.replaceAll(/\D/g, '') || 15);
     const offsetLeft = $container.offset().left + paddingLeft;
     const offsetTop = $container.offset().top;
 
