72923dcdf407d062b85088038dacd6f695233a7b
hapk <96176933+hapk-dls@users.noreply.github.com>
2023-04-03 07:11:17 +0700

    add indexs page

--- source/modules/swiper/swiper.js
+++ source/modules/swiper/swiper.js
@@ -28,6 +28,7 @@ function addSwiper(selector, options = {}) {
 $(function() {
   addSwiper('.banner-slider', {
     pagination: true,
+    navigation: true,
     spaceBetween: 16,
     speed: 500,
     loop: true,
@@ -35,6 +36,55 @@ $(function() {
     autoplay: {
       delay: 4000,
       disableOnInteraction: false,
+    }
+  });
+});
+
+$(function() {
+  addSwiper('.shop-slider', {
+    navigation: true,
+    pagination: true,
+    loop: true,
+    slidesPerView: 2.5,
+    spaceBetween: 16,
+    speed: 400,
+    breakpoints: {
+      576: {
+        slidesPerView: 3
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      992: {
+        slidesPerView: 5,
+      },
+      1200: {
+        slidesPerView: 6,
+        spaceBetween: 30,
+      }
+    }
+  });
+});
+
+$(function() {
+  addSwiper('.card-slider', {
+    navigation: true,
+    pagination: true,
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 16,
+    speed: 400,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+      992: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      }
     }
   });
 });
