960b4d1058fbd9de2e01cb9bf40202bc102a5155
hapk <96176933+hapk-dls@users.noreply.github.com>
2023-04-04 07:55:37 +0700

    add product pages

--- source/js/_common.js
+++ source/js/_common.js
@@ -5,3 +5,55 @@ $(function() {
     }
   });
 });
+
+$(function() {
+  $('.js-number-input').on('input', function() {
+    const value = $(this).val();
+    let newVal = value.replace(/[^0-9]/g, '');
+
+    if (newVal !== '') {
+      newVal = Number(newVal).toLocaleString('en-US');
+    }
+
+    $(this).val(newVal);
+  });
+});
+
+$(function () {
+  // number input
+  $(document).on(
+    "input",
+    "[data-type='number'], .js-input-number",
+    function () {
+      var val = $(this).val();
+      var newVal = val.replace(/[^0-9.]/g, "").replace(/(\..*)\./g, "$1");
+
+      $(this).val(newVal);
+    }
+  );
+
+  // quantity
+  $(document).on("click", ".quantity__btn", function (e) {
+    e.preventDefault();
+
+    var $siblingInput = $(this).siblings(".quantity__input");
+    var plus = $(this).data("plus");
+    var value = $siblingInput.val();
+    var newValue = parseInt(value) + plus;
+    var min = $siblingInput.data("min") || 1;
+
+    if (newValue >= min) {
+      $siblingInput.val(newValue);
+      $siblingInput.trigger("change");
+    }
+  });
+
+  $(document).on("blur", ".quantity__input", function (e) {
+    e.preventDefault();
+
+    if ($(this).val() === "" || $(this).val() === "0") {
+      $(this).val(1);
+      $(this).trigger("change");
+    }
+  });
+});
