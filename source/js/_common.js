68a427793ddb6ef0ed00ff527ba2cf99944f493e
hapk <96176933+hapk-dls@users.noreply.github.com>
2023-04-05 22:14:09 +0700

    add cart page

--- source/js/_common.js
+++ source/js/_common.js
@@ -57,3 +57,75 @@ $(function () {
     }
   });
 });
+
+$(function() {
+  $('.js-cart-check-all').on('change', function() {
+    const checked = $(this).prop('checked');
+    console.log('checked', checked);
+    $('.js-cart-check-item').prop('checked', checked).trigger('change');
+  });
+
+  $('.js-cart-check-group').on('change', function() {
+    const checked = $(this).prop('checked');
+    const $itemCheckbox = $(this).closest('.cart__group').find('.js-cart-check-item');
+    $itemCheckbox.prop('checked', checked).trigger('change');
+  });
+
+  $('.js-cart-check-item, .js-cart-quantity').on('change', function() {
+    calcSubtotal();
+  });
+
+  $('.js-cart-delete-item').on('click', function() {
+    const $group = $(this).closest('.cart__group');
+    const $item = $(this).closest('.cart__row');
+
+    $item.remove();
+
+    if (!$group.find('.js-cart-item').length) {
+      $group.remove();
+    }
+
+    calcSubtotal();
+  });
+
+  $('.js-cart-delete-selected-items').on('click', function(e) {
+    e.preventDefault();
+    $('.js-cart-check-item:checked').closest('.js-cart-item').find('.js-cart-delete-item').trigger('click');
+  });
+
+  calcSubtotal();
+
+  function calcSubtotal() {
+    let itemCount = 0;
+    let subtotal = 0;
+    let isAllChecked = true;
+
+    $('.cart__group').each(function() {
+      const $el = $(this);
+      const $items = $el.find('.js-cart-item');
+      let isChecked = true;
+
+      $items.each(function() {
+        const unitPrice = Number($(this).data('unit-price'));
+        const quantity = $(this).find('.js-cart-quantity').val() || 1;
+        const value = unitPrice * quantity;
+
+        if (!$(this).find('.js-cart-check-item').prop('checked')) {
+          isChecked = false;
+          isAllChecked = false;
+        }
+
+        subtotal += value;
+        itemCount += 1;
+
+        $(this).find('.js-cart-value').html(`${value.toLocaleString('en')} ₫`);
+      });
+
+      $el.find('.js-cart-check-group').prop('checked', isChecked);
+    });
+
+    $('.js-cart-check-all').prop('checked', isAllChecked);
+    $('.js-cart-item-count').text(itemCount);
+    $('.js-cart-subtotal').text(`${subtotal.toLocaleString('en')} ₫`);
+  }
+});
