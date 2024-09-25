f9293e0187de859003895bc115a047d663e148da
HaPK <hapk@dlsinc.com>
2024-09-25 08:00:36 +0700

    add search page

--- source/js/_common.js
+++ source/js/_common.js
@@ -59,6 +59,15 @@ $(function () {
   });
 
   createPostMenu();
+
+  $('.js-daterangepicker').daterangepicker({
+    timePicker: true,
+    startDate: moment().startOf('hour'),
+    endDate: moment().startOf('hour').add(32, 'hour'),
+    locale: {
+      format: 'M/DD hh:mm A'
+    }
+  });
 });
 
 function createPostMenu() {
