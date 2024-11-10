d8ba85f31ad47e108c634bbddc8fecfd41b92eea
HaPK <hapk@dlsinc.com>
2024-11-10 16:25:24 +0700

    add register page

--- source/js/_common.js
+++ source/js/_common.js
@@ -33,3 +33,66 @@ $(function() {
 $(function() {
     $('.js-datepicker').datepicker();
 });
+
+$(function() {
+    const $form = $('.js-register-form');
+    const $modal = $('.md-confirm');
+
+    if (!$form.length || !$modal.length) return;
+
+    const $previewIdFront = $('.js-preview-id-front');
+    const $previewIdBack = $('.js-preview-id-back');
+    const $previewBill = $('.js-preview-bill');
+
+    const $mdPreviewIdFront = $('.js-modal-preview-id-front');
+    const $mdPreviewIdBack = $('.js-modal-preview-id-back');
+    const $mdPreviewBill = $('.js-modal-preview-bill');
+
+    $('.js-open-confirm-modal').on('click', function(e) {
+        e.preventDefault();
+
+        $form.serializeArray().forEach(({ name, value }) => {
+            const $field = $modal.find(`.md-confirm__field[data-name="${name}"]`);
+
+            if (!$field.length) return;
+
+            $field.find('strong').text(value);
+        });
+
+        $mdPreviewIdFront.empty().append($previewIdFront.children().clone());
+        $mdPreviewIdBack.empty().append($previewIdBack.children().clone());
+        $mdPreviewBill.empty().append($previewBill.children().clone());
+
+        $modal.modal('show');
+    });
+
+    $('.js-modal-confirm-submit').on('click', function() {
+        $form.submit();
+    });
+});
+
+$(function() {
+    $('.js-file-input').on('change', function (event) {
+        const target = $(this).data('preview');
+        const $target = $(target);
+
+        if (!$target.length) return;
+
+        $target.empty();
+
+        const file = event.target.files[0];
+
+        console.log('file', file);
+
+        if (!file) return;
+
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const url = e.target.result;
+            $target.append(`<img src="${url}" alt="" />`);
+        };
+
+        reader.readAsDataURL(file);
+    });
+})
