e5e8ccdd7ec29065c825f5a7cea68daae2001003
HaPK <hapk@dlsinc.com>
2025-01-19 16:34:40 +0700

    draw routes

--- source/js/map.js
+++ source/js/map.js
@@ -1,4 +1,6 @@
 let map;
+let directionsService;
+let directionsRenderer;
 let infoWindows = [];
 
 async function initMap() {
@@ -11,6 +13,46 @@ async function initMap() {
         center: { lat: 0, lng: 0 },
         mapTypeId: google.maps.MapTypeId.SATELLITE,
     });
+
+    directionsService = new google.maps.DirectionsService();
+
+    // Create a DirectionsRenderer object
+    directionsRenderer = new google.maps.DirectionsRenderer();
+}
+
+function drawRoute(waypoints) {
+    if (directionsRenderer) {
+        console.log("set map null");
+        directionsRenderer.setMap(null); // Remove the route from the map
+    }
+
+    if (waypoints.length < 2) return;
+
+    directionsRenderer.setMap(map);
+
+    const start = waypoints.shift();
+    const end = waypoints.pop();
+
+    waypoints = waypoints.map(loc => ({ location: loc, stopover: true }));
+
+    // Request for driving directions
+    directionsService.route(
+        {
+            origin: start,
+            destination: end,
+            waypoints: waypoints,
+            travelMode: google.maps.TravelMode.DRIVING, // Travel mode
+            optimizeWaypoints: true,
+        },
+        (response, status) => {
+            if (status === "OK") {
+                // Render the route on the map
+                directionsRenderer.setDirections(response);
+            } else {
+                console.error("Directions request failed due to " + status);
+            }
+        }
+    );
 }
 
 function removeAllInfoWindows() {
@@ -22,7 +64,11 @@ function removeAllInfoWindows() {
 }
 
 function renderMarkersAndCentering(locations) {
+    console.log('renderMarkersAndCentering', locations);
+
     const bounds = new google.maps.LatLngBounds();
+
+    let waypoints = [];
 
     locations.forEach(location => {
         const marker = new google.maps.Marker({
@@ -59,10 +105,17 @@ function renderMarkersAndCentering(locations) {
         bounds.extend(marker.getPosition());
 
         infoWindows.push(infoWindow);
+
+        waypoints.push({
+            lat: location.lat,
+            lng: location.lng,
+        });
     })
 
     // Fit the map's viewport to the bounds
     map.fitBounds(bounds);
+
+    drawRoute(waypoints);
 }
 
 function renderDefaultRoutes() {
